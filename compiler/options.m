2aedf839698d3e9a08aed136248908108efc1c8d
Paul Bone <paul@bone.id.au>
2017-02-03 16:00:29 +1100

    Add new --warn-non-tail-recursion options
    Add --warn-non-tail-recursion-self and --warn-non-tail-recursion-mutual and
    make --warn-non-tail-recursion imply both of these options.
    
    Currently --warn-non-tail-recursion implements only
    --warn-non-tail-recursion-self.
    
    compiler/options.m:
        Add new options.
    
    compiler/handle_options.m:
        Make --warn-non-tail-recursion imply the two new options.
    
    doc/user_guide.texi:
        Document the new options.

--- compiler/options.m
+++ compiler/options.m
@@ -2,6 +2,7 @@
 % vim: ft=mercury ts=4 sw=4 et
 %---------------------------------------------------------------------------%
 % Copyright (C) 1994-2012 The University of Melbourne.
+% Copyright (C) 2017 The Mercury Team.
 % This file may only be copied under the terms of the GNU General
 % Public License - see the file COPYING in the Mercury distribution.
 %---------------------------------------------------------------------------%
@@ -170,6 +171,8 @@
     ;       warn_smart_recompilation
     ;       warn_undefined_options_variables
     ;       warn_non_tail_recursion
+    ;       warn_non_tail_recursion_self
+    ;       warn_non_tail_recursion_mutual
     ;       warn_target_code
     ;       warn_up_to_date
     ;       warn_stubs
@@ -1157,6 +1160,8 @@ option_defaults_2(warning_option, [
     warn_smart_recompilation            -   bool(yes),
     warn_undefined_options_variables    -   bool(yes),
     warn_non_tail_recursion             -   bool(no),
+    warn_non_tail_recursion_self        -   bool(no),
+    warn_non_tail_recursion_mutual      -   bool(no),
     warn_target_code                    -   bool(yes),
     warn_up_to_date                     -   bool(yes),
     warn_stubs                          -   bool(yes),
@@ -2043,6 +2048,10 @@ long_option("warn-undefined-options-variables",
 long_option("warn-undefined-options-vars",
                     warn_undefined_options_variables).
 long_option("warn-non-tail-recursion",  warn_non_tail_recursion).
+long_option("warn-non-tail-recursion-self",
+                    warn_non_tail_recursion_self).
+long_option("warn-non-tail-recursion-mutual",
+                    warn_non_tail_recursion_mutual).
 long_option("warn-target-code",         warn_target_code).
 long_option("warn-up-to-date",          warn_up_to_date).
 long_option("warn-stubs",               warn_stubs).
@@ -3131,7 +3140,8 @@ special_handler(Option, SpecialData, !.OptionTable, Result) :-
                     warn_wrong_module_name          -   bool(Enable),
                     warn_smart_recompilation        -   bool(Enable),
                     warn_undefined_options_variables -  bool(Enable),
-                    warn_non_tail_recursion         -   bool(Enable),
+                    warn_non_tail_recursion_self    -   bool(Enable),
+                    warn_non_tail_recursion_mutual  -   bool(Enable),
                     warn_target_code                -   bool(Enable),
                     warn_up_to_date                 -   bool(Enable),
                     warn_stubs                      -   bool(Enable),
@@ -3161,7 +3171,8 @@ special_handler(Option, SpecialData, !.OptionTable, Result) :-
                     warn_simple_code                -   bool(Enable),
                     warn_duplicate_calls            -   bool(Enable),
                     warn_implicit_stream_calls      -   bool(Enable),
-                    warn_non_tail_recursion         -   bool(Enable),
+                    warn_non_tail_recursion_self    -   bool(Enable),
+                    warn_non_tail_recursion_mutual  -   bool(Enable),
                     warn_dead_procs                 -   bool(Enable),
                     warn_dead_preds                 -   bool(Enable),
                     warn_known_bad_format_calls     -   bool(Enable),
@@ -3741,7 +3752,12 @@ options_help_warning -->
         "\tDo not warn about references to undefined variables in",
         "\toptions files with `--make'.",
         "--warn-non-tail-recursion",
+        "\tWarn about any recursive calls that are not tail calls,",
+        "\timplies the next two options.",
+        "--warn-non-tail-recursion-self",
         "\tWarn about any directly recursive calls that are not tail calls.",
+        "--warn-non-tail-recursion-mutual",
+        "\tWarn about any mutually recursive calls that are not tail calls.",
         "--no-warn-up-to-date",
         "\tDo not warn if targets specified on the command line",
         "\twith `--make' are already up-to-date.",
